618a7c80b0d9b3d61642486f1c502332297292b6
manikandanraji <manikandan4871113@gmail.com>
2020-05-16 15:12:16 +0530

    pushing to production

--- src/api/User/profile/profile.js
+++ src/api/User/profile/profile.js
@@ -1,8 +1,14 @@
 const { PROFILE_FRAGMENT } = require("../../../utils/fragments");
 
 module.exports = {
-  Query: {
-    profile: (parent, args, ctx) =>
-      ctx.prisma.user({ handle: args.handle }).$fragment(PROFILE_FRAGMENT),
-  },
+	Query: {
+		profile: async (parent, args, ctx) => {
+			const userExists = await ctx.prisma.$exists.user({ handle: args.handle });
+
+			if (!userExists) throw Error(`No user found for handle - ${args.handle}`);
+			return ctx.prisma
+				.user({ handle: args.handle })
+				.$fragment(PROFILE_FRAGMENT);
+		}
+	}
 };
