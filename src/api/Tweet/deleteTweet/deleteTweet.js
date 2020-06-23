9782e42d04db6b121eabd17926d6569820f4c3f3
manikandanraji <manikandan4871113@gmail.com>
2020-06-23 17:10:26 +0530

    fixed deleteTweet security

--- src/api/Tweet/deleteTweet/deleteTweet.js
+++ src/api/Tweet/deleteTweet/deleteTweet.js
@@ -1,3 +1,5 @@
+const { TWEET_FRAGMENT } = require("../../../utils/fragments");
+
 module.exports = {
   Mutation: {
     deleteTweet: async (parent, args, ctx) => {
@@ -6,12 +8,16 @@ module.exports = {
       if (!userId) throw Error("You need to be authenticated");
 
       // 2. check if the tweet exists
-      const exists = await ctx.prisma.$exists.tweet({ id: args.id });
-      if (!exists) {
-        throw Error(`No tweet exists for this id - ${args.id}`);
-      }
+			const tweet = await ctx.prisma.tweet({ id: args.id }).$fragment(TWEET_FRAGMENT)
+			if(!tweet) {
+				throw Error(`No tweet for found id - ${args.id}`)
+			}
 
-      // 3. return the deleted tweet
+			// 3. make sure the tweet belongs to the user
+			if(tweet.user.id !== userId) {
+				throw Error("You don't have permission to delete this tweet")
+			}
+
       return ctx.prisma.deleteTweet({ id: args.id });
     },
   },
